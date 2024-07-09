a196c3a2b10facd6044ddd02dbb5aaca02a7aaca
heidless <rob.lockhart@yahoo.co.uk>
2024-07-09 16:44:34 +0100

    update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => Rails.application.credentials.sendgrid_mailer[:user_name],
+  :password => Rails.application.credentials.sendgrid_mailer[:api_key_secret], # secret sendgrid API key
+  :domain => Rails.application.credentials.sendgrid_mailer[:domain],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,   # 465	(for SSL connections). 25, 587	(for unencrypted/TLS connections)
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+
