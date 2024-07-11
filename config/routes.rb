18ec75b288b980827a8b757ae285b1532ef2d9b3
heidless <rob.lockhart@yahoo.co.uk>
2024-07-11 10:46:12 +0100

    intial homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get 'home/index'
-  devise_for :users
+  resources :images
+  devise_for :users, :controllers => { :registrations => 'registrations' }
+  root 'welcome#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# Rails.application.routes.draw do
+#   root 'welcome#index'
+#   get 'home/index'
+#   devise_for :users
+#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# end
+
