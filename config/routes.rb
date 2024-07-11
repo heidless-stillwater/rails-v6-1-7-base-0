c0f49be07c494cfeb5037af0f6c0aeca0a98e952
heidless <rob.lockhart@yahoo.co.uk>
2024-07-11 14:15:42 +0100

    stripe installed & configured

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,6 @@
 Rails.application.routes.draw do
   resources :images
-#  devise_for :users, :controllers => { :registrations => 'registrations' }
-  devise_for :users
+  devise_for :users, :controllers => { :registrations => 'registrations' }
   root 'welcome#index' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
-
-# Rails.application.routes.draw do
-#   root 'welcome#index'
-#   get 'home/index'
-#   devise_for :users
-#   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-# end
-
